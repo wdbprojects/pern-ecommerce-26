2b0b1714a4a8178e795d8d41a1e81737e315134f
Ronaldo Ortiz <ronyortizop@gmail.com>
2026-05-09 23:51:39 -0400

    fixed ENV source

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "drizzle-kit";
-import { ENV } from "./src/config/env";
+import { getEnv } from "./src/config/env";
+
+const ENV = getEnv();
 
 export default defineConfig({
   out: "./src/drizzle/migrations",
   schema: "./src/drizzle/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: ENV.DATABASE_URL!,
+    url: ENV.DATABASE_URL,
   },
 });
