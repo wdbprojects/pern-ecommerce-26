39625180bc873af23e2a706c4aad74351894a3bc
Ronaldo Ortiz <ronyortizop@gmail.com>
2026-05-22 11:29:17 -0400

    11-react-query-plus-frontend-setup

---
+++ frontend/src/app/loading.tsx
@@ -0,0 +1,15 @@
+import { Spinner } from "@/components/ui/spinner";
+
+const LoadingPage = async () => {
+  return (
+    <div className="flex h-[calc(100vh-4rem)] w-full items-center justify-center">
+      <Spinner className="text-primary size-18" />
+    </div>
+  );
+};
+
+export default LoadingPage;
+
+{
+  /* <CircleDashed color="#fa7f26" strokeWidth={1.25} /> */
+}
