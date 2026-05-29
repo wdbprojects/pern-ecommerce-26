cdc9e9eeefab1f98f6146100c4831b9d82576c06
Ronaldo Ortiz <ronyortizop@gmail.com>
2026-05-29 12:27:55 -0400

    fixed 'asChild' from nav-user

--- frontend/src/modules/components/sidebar/nav-user.tsx
+++ frontend/src/modules/components/sidebar/nav-user.tsx
@@ -27,26 +27,29 @@ const NavUser = () => {
     <SidebarMenu>
       <SidebarMenuItem>
         <DropdownMenu>
-          <DropdownMenuTrigger className="w-full" asChild>
-            <SidebarMenuButton
-              size="lg"
-              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground w-full cursor-pointer"
-              tooltip="User Settings"
-            >
-              <Avatar className="h-8 w-8 rounded-lg">
-                <AvatarImage
-                  src={user.avatar ? user.avatar : ""}
-                  alt={user.name}
-                  className="rounded-lg"
-                />
-                <AvatarFallback>SJ</AvatarFallback>
-              </Avatar>
-              <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-medium">{user.name}</span>
-                <span className="truncate text-xs">{user.email}</span>
-              </div>
-            </SidebarMenuButton>
-          </DropdownMenuTrigger>
+          <DropdownMenuTrigger
+            className="w-full"
+            render={
+              <SidebarMenuButton
+                size="lg"
+                className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground w-full cursor-pointer"
+                tooltip="User Settings"
+              >
+                <Avatar className="h-8 w-8 rounded-lg">
+                  <AvatarImage
+                    src={user.avatar ? user.avatar : ""}
+                    alt={user.name}
+                    className="rounded-lg"
+                  />
+                  <AvatarFallback>SJ</AvatarFallback>
+                </Avatar>
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <span className="truncate font-medium">{user.name}</span>
+                  <span className="truncate text-xs">{user.email}</span>
+                </div>
+              </SidebarMenuButton>
+            }
+          ></DropdownMenuTrigger>
           <DropdownMenuGroup>
             <DropdownMenuContent
               className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
